293de03a2a8ac572664f0d8c917c4370f37d7a3e
Oleg4000 <olegklim568@gmail.com>
2023-02-14 22:01:31 +0200

    change cofig files acssess for db2

--- todo_config.php
+++ todo_config.php
@@ -1,5 +1,5 @@
 <?php
-//$connect = mysqli_connect('srvservice32','ODW_UA_EDV','ODW_UA_EDV','ODW_TEST');
+//$connect = mysqli_connect('srvservice32','ODW_UA_EDV','ODW_UA_EDV','ODW_TEST');  //for odw db
 $connect =mysqli_connect('localhost', 'to_do_car','root','');
 if(!$connect)
 {
